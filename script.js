d1faaf2dd6653d471161a5f640ec601bf8e3f692
zabdielfavela7 <zabdielfavela7@gmail.com>
2022-02-16 17:13:01 +0000

    added playRound function(takes 2 strings and returns win or lose). added game function (calls playRound and keeps score, wins the first player to reach 3 victories)

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function computerPlay(){
 function playerSelection(){
     let selectionPlayer = prompt("Enter (R)Rock, (P)paper or (S)sicssors");
     selectionPlayer = selectionPlayer.toUpperCase();
-    console.log(selectionPlayer);
 
     if(selectionPlayer == 'R'){
         return "ROCK"
@@ -21,5 +20,70 @@ function playerSelection(){
 
 }
 
+function playRound(player, computer){
+    console.log("player plays " + player);
+    console.log("computer playes " + computer);
 
-console.log(playerSelection());
+    if(player == computer){
+        console.log("Tie on " + player );
+    } else {
+        if(player == "ROCK"){
+            if(computer == "SICSSORS"){
+                console.log("win " + player + " beats " + computer);
+                return "win"
+            } else{
+                console.log("lose " + computer + " beats " + player);
+                return "lose"
+            }
+        }
+        if(player == "PAPER"){
+            if(computer == "ROCK"){
+                console.log("win " + player + " beats " + computer);
+                return "win"
+            } else{
+                console.log("lose " + computer + " beats " + player);
+                return "lose"
+            }
+        }
+        if(player == "SICSSORS"){
+            if(computer == "PAPER"){
+                console.log("win " + player + " beats " + computer);
+                return "win"
+            } else{
+                console.log("lose " + computer + " beats " + player);
+                return "lose"
+            }
+        }
+    }
+}
+
+function game(){
+
+    alert("The player with 3 victories wins")
+    let scorePlayer = 0;
+    let scoreComputer = 0;
+
+    while(scorePlayer < 3 && scoreComputer < 3){
+        let result = playRound(playerSelection(), computerPlay());
+        
+        if(result == "win"){
+            scorePlayer++;
+        } else if(result == "lose"){
+            scoreComputer++;
+        }
+
+        console.log("score is " + scorePlayer + "-" + scoreComputer);
+        
+    }
+
+    if(scorePlayer > scoreComputer){
+        alert("YOU HAVE WON!!!");
+    }else{
+        alert("you lost :(");
+    }
+
+}
+
+
+
+game();

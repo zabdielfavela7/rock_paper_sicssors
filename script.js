6b661f63ef3cf009fb33beffa8a9597a8576355d
zabdielfavela7 <zabdielfavela7@gmail.com>
2022-03-25 16:33:06 +0000

    funcionality with buttons

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
+let scorePlayer = 0;
+let scoreComputer = 0;
+
 const selection = ["ROCK","PAPER","SICSSORS"];
+
+const container = document.querySelector('#container');
+const score = document.createElement('div');
+score.textContent = "0-0"
+container.appendChild(score);
+const div = document.createElement('div');
+container.appendChild(div);
+
+selection.forEach( ele => {
+    let btn = document.createElement("button");
+    btn.innerText = ele;
+    btn.value = ele;
+
+    btn.addEventListener('click', () => {
+        playRound(btn.value, computerPlay());
+    });
+
+    container.appendChild(btn);
+});
+
+
 
 function computerPlay(){    
     return selection[Math.floor(Math.random()*3)]
 }
 
+/*
 function playerSelection(){
     let selectionPlayer = prompt("Enter (R)Rock, (P)paper or (S)sicssors");
     selectionPlayer = selectionPlayer.toUpperCase();
@@ -18,45 +43,60 @@ function playerSelection(){
         return playerSelection()
     }
 
-}
+}*/
 
 function playRound(player, computer){
     console.log("player plays " + player);
     console.log("computer playes " + computer);
 
+    
+
     if(player == computer){
-        console.log("Tie on " + player );
+        div.textContent = "Tie on " + player;
     } else {
         if(player == "ROCK"){
             if(computer == "SICSSORS"){
-                console.log("win " + player + " beats " + computer);
-                return "win"
+                div.textContent = "win " + player + " beats " + computer;
+                scorePlayer++;
             } else{
-                console.log("lose " + computer + " beats " + player);
-                return "lose"
+                div.textContent = "lose " + computer + " beats " + player;
+                scoreComputer++;
             }
         }
         if(player == "PAPER"){
             if(computer == "ROCK"){
-                console.log("win " + player + " beats " + computer);
-                return "win"
+                div.textContent = "win " + player + " beats " + computer;
+                scorePlayer++;
             } else{
-                console.log("lose " + computer + " beats " + player);
-                return "lose"
+                div.textContent = "lose " + computer + " beats " + player;
+                scoreComputer++;
             }
         }
         if(player == "SICSSORS"){
             if(computer == "PAPER"){
-                console.log("win " + player + " beats " + computer);
-                return "win"
+                div.textContent = "win " + player + " beats " + computer;
+                scorePlayer++;
             } else{
-                console.log("lose " + computer + " beats " + player);
-                return "lose"
+                div.textContent =   "lose " + computer + " beats " + player;
+                scoreComputer++;
             }
         }
     }
-}
+    
+    score.textContent = scorePlayer + "-" + scoreComputer;
 
+    if(scorePlayer == 5){
+        const winner = document.createElement('div');
+        winner.textContent = "you WIN";
+        container.appendChild(winner);
+    } 
+    if(scoreComputer == 5){
+        const winner = document.createElement('div');
+        winner.textContent = "you LOSE";
+        container.appendChild(winner);
+    }
+}
+/*
 function game(){
 
     alert("The player with 3 victories wins")
@@ -83,8 +123,8 @@ function game(){
     }
 
 }
+*/
 
 
-
-game();
+//game();
 //temp branch commit
